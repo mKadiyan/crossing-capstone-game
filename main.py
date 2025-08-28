e6d93bf37cf7449570748ec3d3bd6c45caffc2cb
Ravinder Kadiyan <ravinder.kadiyan@gmail.com>
2025-08-28 13:05:29 +0530

    reset player once it goto finish line

--- main.py
+++ main.py
@@ -21,7 +21,14 @@ while game_is_on:
     time.sleep(0.2)
     screen.update()
 
-
     car_manager.create_car()
     car_manager.move_cars()
 
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+
+    if player.is_at_finish_line():
+        player.go_to_start()
+
+screen.exitonclick()
--- player.py
+++ player.py
@@ -9,8 +9,16 @@ class Player(Turtle):
         super().__init__()
         self.shape("turtle")
         self.penup()
-        self.goto(STARTING_POINT)
+        self.go_to_start()
         self.setheading(90)
 
     def go_up(self):
         self.forward(MOVE_DISTANCE)
+
+    def is_at_finish_line(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        return False
+
+    def go_to_start(self):
+        self.goto(STARTING_POINT)
