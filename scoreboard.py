d5a0c85f8511b436368c6713228c4d0fe0835406
Ravinder Kadiyan <ravinder.kadiyan@gmail.com>
2025-08-28 13:18:56 +0530

    Add score and game over messages

--- scoreboard.py
+++ scoreboard.py
@@ -1,4 +1,30 @@
-FONT = ("Courier", 24, "normal")
+from turtle import Turtle
 
-class Scoreboard:
-    pass
+
+FONT = ("Courier", 12, "normal")
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("black")
+        self.penup()
+        self.hideturtle()
+        self.level = 0
+        self.update_score()
+
+    def update_score(self):
+        self.clear()
+        self.goto(-250, 280)
+        self.write(f"Level: {self.level}", align="center", font=FONT)
+
+    def level_up(self):
+        self.level += 1
+        self.update_score()
+
+    def reset(self):
+        self.level = 0
+        self.update_score()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"Game Over", align="center", font=("Arial", 20, "bold"))
